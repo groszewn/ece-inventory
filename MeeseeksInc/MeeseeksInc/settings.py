cc7ef291d2a94125020719647aac0ae457066867
nrg12 <nrg12@duke.edu>
2017-01-18 11:35:06 -0500

    changed db to postgres

--- MeeseeksInc/MeeseeksInc/settings.py
+++ MeeseeksInc/MeeseeksInc/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'MeeseeksInc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'inventory',
+	'USER': 'meeseeks_admin',
+	'PASSWORD':'meeseeks-admin', 
+	'HOST':'localhost', 
+	'PORT':'',
     }
 }
 
