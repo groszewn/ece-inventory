46ae7b01a273b62d1c729c1a6b7d79372d24e87c
nrg12 <nrg12@duke.edu>
2017-01-24 19:37:26 -0500

    requiring login to access any pages

--- MeeseeksInc/MeeseeksInc/urls.py
+++ MeeseeksInc/MeeseeksInc/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^', include('inventory.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, {'template_name': 'inventory/login.html'}, name='login'), 
+    url(r'^logout/$', auth_views.logout, {'template_name': 'inventory/logged_out.html'}, name='logout'),
 ]
