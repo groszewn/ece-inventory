912146914c7f6d7bda5c6c4520b9837d68e1cd38
nrg12 <nrg12@duke.edu>
2017-03-28 05:15:11 -0400

    all done for ev3

--- MeeseeksInc/MeeseeksInc/settings.py
+++ MeeseeksInc/MeeseeksInc/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x6s74m_9(u*#)ntmwf0g&1xpwh^7#x4m$07-*xzq4=l^qg!rhw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['colab-sbx-48.oit.duke.edu', 'colab-sbx-248.oit.duke.edu','meeseeksinc.colab.duke.edu', '127.0.0.1','localhost','colab-sbx-134.oit.duke.edu', 'meeseeks.colab.duke.edu','152.3.53.138']
 
