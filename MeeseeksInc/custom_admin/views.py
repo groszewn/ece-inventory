2a81858d500e367b9419f6f3f721059002abc2b5
nrg12 <nrg12@duke.edu>
2017-04-18 00:15:13 -0400

    working asset loan edit

--- MeeseeksInc/custom_admin/views.py
+++ MeeseeksInc/custom_admin/views.py
@@ -361,6 +361,10 @@ class LoanView(LoginRequiredMixin, UserPassesTestMixin, generic.ListView):
                 token, create = Token.objects.get_or_create(user=user)
                 http_host = get_host(request)
                 url=http_host+'/api/loan/update/'+loan.loan_id+'/'
+                if loan.item_name.is_asset:
+                    quantity = loan.total_quantity
+                else:
+                    quantity = post.total_quantity
                 payload = {'comment': post.comment,'total_quantity':post.total_quantity}
                 header = {'Authorization': 'Token '+ str(token), 
                       "Accept": "application/json", "Content-type":"application/json"}
